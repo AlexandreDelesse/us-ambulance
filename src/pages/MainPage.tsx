2b655816a71e4efbbfb853cc8d722bffba5e5157
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2025-07-02 15:47:51 +0200

    Clean for build

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,7 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import TopAppBar from "../Components/Nav/TopAppBar";
 import { Outlet } from "react-router";
 import NotificationModal from "../Components/Notification/NotificationModal";
-import { useUser } from "../Components/User/UserContext";
-import WaitingVerif from "../assets/attente-verification.png";
 
 export default function MainPage() {
   return (
